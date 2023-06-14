73dcf9a2b3d2851675818e8d3ceadc45bb9920e7
changs3 <sio.chang3@mail.dcu.ie>
2023-06-14 15:19:36 +0100

    Add an event handler

--- simplistic-website/app.js
+++ simplistic-website/app.js
@@ -1,1 +1,21 @@
 'use strict';
+// switcher is now a reference to the button in the page.
+const switcher = document.querySelector('.btn');
+
+// add the event handler for the click event. 
+// In the following code, you add a listener for the click event and 
+// define an event handler function to be executed by the browser when the click event
+//  occurs.
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+
+    const className = document.body.className;
+    if (className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+    console.log('current class name: ' + className);
+
+});
